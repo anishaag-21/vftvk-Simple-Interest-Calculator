e4e7a968088d86281b1c4eff74b43bad5fdc16ce
anishaag-21 <96198785+anishaag-21@users.noreply.github.com>
2022-03-29 02:01:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
-function compute()
-{
-    var principal=document.getElementById("principal").value;
-    var rate=document.getElementById("interest_rate").value;
-    var years =document.getElementById("years").value;
-    var interest = principal*years*rate/100;
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * year * rate / 100
 
-    if (principal=="") //validating non-zero value
-    {
-        
-        alert("Please enter a positive number");
-        document.getElementById("principal").value='';
-        document.getElementById("principal").focus();
-    }
-    else
-    {
-        var d = new Date();
-        var y = d.getFullYear();
-        var future_year = Number(y) + Number(years);
-        var result = document.getElementById("result");
-        result.innerHTML="If you deposit <mark>" + principal +"<mark>,<br/> at an interest rate of <mark>"
-        +rate+"%<mark>,<br/> You will receive an amount of <mark>"+ interest+
-        "<mark>,<br/>in the year<mark>"+future_year+"</mark><br/>"
-    }
-}
-        
-//To display slider value
-function slider()
-{
-    var rate=document.getElementById("interest_rate");
-    var output=document.getElementById("showrate");
-    output.innerHTML = rate.value
 
-    rate.oninput=function()
-    {
-        output.innerHTML=this.value
-    }
-}       
+// function that validates the principal input box
+function principal_validation() {
+    var alphaExp = /^[a-zA-Z]+$/;
+    pr = document.getElementById("principal").value;
+    if (pr <= 0 || pr == "" || (alphaExp.test(pr) == true)) {
+        alert("Enter a positive number");
+        setTimeout("document.getElementById('principal').focus();", 2);
+        document.getElementById('principal').value = "";
+        document.getElementById("result").innerHTML = "";
 
-//to validate principal amount
-
-function positive_value()
-{
-    var principal = document.getElementById("principal").value
-    if (Number (principal)<=0)
-    {
-        alert("Please enter a positive number");
-        document.getElementById("principal").value='';
-        document.getElementById("principal").focus();
         return false;
     }
-    else 
-    {
-        return true
-    }
+}
+
+
+// function that reads the value of the range slider
+// and displays it the <span>adjacent to the slider
+function range() {
+    r = document.getElementById("rate").value;
+    document.getElementById('spanRate').innerHTML = r + "%";
+}
+
+function compute() {
+
+    // Get principal from principal validation function
+    principal_validation();
+    principal = pr;
+
+    // Get rate from range function
+    range();
+    rate = r;
+
+    // Get years from dropdown menu
+    var y = document.getElementById("years");
+    years = y.options[y.selectedIndex].value;
+
+    // Calculate interest
+    interest = principal * rate * years / 100;
+
+    // Logic to convert the 'No of Years' into the actual
+    // year in the future.
+    const currentYear = new Date().getFullYear();
+    finalYear = (parseInt(currentYear) + parseInt(years));
+
+
+    // Write result message
+    var res = "<br>" + "If you deposit " + "<span class='highlight'>" +
+        principal + "</span>" + "," + "<br>" + "at an interest rate of " +
+        "<span class='highlight'>" + rate + "%" + "</span>" + "." + "<br>" +
+        "You will receive an amount of " + "<span class='highlight'>" +
+        interest + "</span>" + "," + "<br>" +
+        "in the year " + "<span class='highlight'>" + finalYear + "</span>" +
+        "." + "<br>";
+
+    result = "";
+    result = document.getElementById("result");
+
+    result.innerHTML = res;
+
+
+
 }
